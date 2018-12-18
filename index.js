ea01bc40512a3ffae62c3d2495afd544ab0baa22
Thomas Hansen <ctrhansen@yahoo.com>
2018-12-18 18:05:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var cart = { 
-    itemName: "itemName", itemPrice: (math.random(1 - 100)) ,}; 
+    itemName: "itemName", itemPrice: (math.random() * math.) ,}; 
     for (var i = 0; item.length > i; i++) {
       
     }
